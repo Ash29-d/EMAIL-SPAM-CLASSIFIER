33c49eac455d7e7b86730564cf799f8d5d9039d0
Ashwin Suthar <ashwinsuthar656@gmail.com>
2024-12-18 13:52:43 +0530

    downloaded stopwords

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns 
 import nltk
 nltk.download('punkt_tab')
+nltk.download('stopwords')
 import regex as re
 from nltk.corpus import stopwords 
 from nltk.stem.porter import PorterStemmer
